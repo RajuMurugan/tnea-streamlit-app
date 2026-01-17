2ec95ca616e5e0f26454acb8e422dbea8688a4aa
RajuMurugan <rajumurugannp@gmail.com>
2026-01-17 07:11:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,19 +140,19 @@ else:
         with col1:
             maths = st.number_input(
                 "✏️ Maths",
-                min_value=0.0, max_value=100.0, value= , step=1.0
+                min_value=0.0, max_value=100.0, value= Enter Maths mark , step=1.0
             )
 
         with col2:
             physics = st.number_input(
                 "⚡ Physics",
-                min_value=0.0, max_value=100.0, value= , step=1.0
+                min_value=0.0, max_value=100.0, value= Enter Physis mark, step=1.0
             )
 
         with col3:
             chemistry = st.number_input(
                 "🧪 Chemistry ",
-                min_value=0.0, max_value=100.0, value= , step=1.0
+                min_value=0.0, max_value=100.0, value= Enter Chemistry mark , step=1.0
             )
 
         calc_btn = st.form_submit_button("✅ Calculate Cutoff")
@@ -609,6 +609,7 @@ elif selected == "TNEA Vacancy Seat Matrix":
 
     if college_df.empty:
         st.warning("⚠️ No data found for the selected college or branch.")
+
 
 
 
